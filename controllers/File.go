661abcfd3514ed0a4ccb377dae421f8a02d0c7c4
HAYASAKARyosuke <hayasaka.ryosuke@gmail.com>
2021-07-24 19:04:04 +0900

    init commit

---
+++ controllers/File.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+	//"encoding/json"
+	//"github.com/HAYASAKA-Ryosuke/simple-file-storage/database"
+)
+
+func FetchFileList(form *http.Request) string {
+	return "hello"
+}
+
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	//"strings"
+	"github.com/HAYASAKA-Ryosuke/simple-file-storage/controllers"
+)
+
+func filesRouter(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //オプションを解析します。デフォルトでは解析しません。
+
+	if r.URL.Path == "/files/" && r.Method == "GET" {
+		data := FetchFileList(r);
+		fmt.Fprintf(w, data) //ここでwに入るものがクライアントに出力されます。
+		return
+	}
+}
+
+func main() {
+//	env := "local"
+//	database.DBInit(env)
+//	db := database.GetDatabase()
+//	defer db.Close()
+
+	// ルーティング
+	http.HandleFunc("/files/", filesRouter)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
